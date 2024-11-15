e485c2092cd805ac376e0c67c16df5c5ba287203
BABARSHAH786 <159919873+BABARSHAH786@users.noreply.github.com>
2024-11-16 00:32:53 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// add to the top
+import React from "react";
+import { createRoot } from "react-dom/client";
+ 
+// modify the createRoot call, delete "ReactDOM"
+// const root = createRoot(container);
+// const Pet = (props) => {
+//     return (
+//       <div>
+//         <h1>{props.name}</h1>
+//         <h2>{props.animal}</h2>
+//         <h2>{props.breed}</h2>
+//       </div>
+//     );
+//   };
+   
+//   export default Pet;
+
+
+export default function Navbar(){
+    return(
+        <div>HELLO</div>
+    )
+}
