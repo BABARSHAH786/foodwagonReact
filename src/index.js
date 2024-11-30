ee82ea199eb626efa37097844f9313d90f122584
BABARSHAH786 <159919873+BABARSHAH786@users.noreply.github.com>
2024-11-30 16:10:13 +0500

    edit vite for fast work

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-// root.render(React.createElement("h2", {}, "We are learning React"));
-root.render(React.createElement(App));
